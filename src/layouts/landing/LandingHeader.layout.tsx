0094b965890239f40ca7a045f3032cefc283d204
Sylegg <plopro09@gmail.com>
2026-02-02 15:29:38 +0700

    fix v3

--- src/layouts/landing/LandingHeader.layout.tsx
+++ src/layouts/landing/LandingHeader.layout.tsx
@@ -1,10 +1,11 @@
     import { useState } from "react";
-import { Link } from "react-router-dom";
-import { ROUTER_URL } from "../../routes/router.const";
-import { Button } from "../../components";
-import logoHylux from "../../assets/logo-hylux.png";
+    import { Link, useNavigate } from "react-router-dom";
+    import { ROUTER_URL } from "../../routes/router.const";
+    import { Button } from "../../components";
+    import logoHylux from "../../assets/logo-hylux.png";
 
-const LandingHeader = () => {
+    const LandingHeader = () => {
+    const navigate = useNavigate();
     const [menuOpen, setMenuOpen] = useState(false);
 
     const scrollToSection = (sectionId: string) => {
