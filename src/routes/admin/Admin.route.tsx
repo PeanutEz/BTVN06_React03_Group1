fb68fba30a97ab725f7282d6d7bf78401e6cfdae
bbao2114 <giabaobao2114@gmail.com>
2026-03-03 01:44:08 +0700

    add change franchise in admin router

--- src/routes/admin/Admin.route.tsx
+++ src/routes/admin/Admin.route.tsx
@@ -3,13 +3,21 @@ import AdminLayout from "../../layouts/admin/Admin.layout";
 import AdminGuard from "../guard/AdminGuard";
 import { ROUTER_URL } from "../router.const";
 import { ADMIN_MENU } from "./Admin.menu";
+import FranchiseCreateEditPage from "@/pages/admin/franchise/FranchiseCreateEdit.page";
 
 export const AdminRoutes = (
   <Route element={<AdminGuard />}>
     <Route path={ROUTER_URL.ADMIN} element={<AdminLayout />}>
+
       {ADMIN_MENU.map((item) => (
         <Route key={item.path} path={item.path} element={<item.component />} />
       ))}
+
+      {/* ✅ Thêm route edit ở đây */}
+     <Route
+      path="franchises/:id/edit"
+      element={<FranchiseCreateEditPage />}
+    />
     </Route>
   </Route>
 );
