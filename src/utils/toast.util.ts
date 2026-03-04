a3267788f9959e5b17d641e372201247e7d80864
Sylegg <plopro09@gmail.com>
2026-03-04 13:02:11 +0700

    reset

--- src/utils/toast.util.ts
+++
@@ -1,8 +0,0 @@
-import { toast } from "sonner";
-
-export const showInfo = (message: string) => toast.info(message);
-export const showSuccess = (message: string) => toast.success(message);
-export const showWarning = (message: string) => toast.warning(message);
-export const showError = (message: string) => toast.error(message);
-export const showLoading = (message: string) => toast.loading(message);
-export const dismissToast = (toastId?: string) => toast.dismiss(toastId);
