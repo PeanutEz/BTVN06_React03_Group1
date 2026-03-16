b2c150bf6c336c0fff84dbb601d4dee82198f4c7
bbao2114 <giabaobao2114@gmail.com>
2026-03-16 10:42:48 +0700

    add voucher in sidebar

--- src/layouts/admin/AdminSidebar.layout.tsx
+++ src/layouts/admin/AdminSidebar.layout.tsx
@@ -105,6 +105,25 @@ const adminNav: Array<{
     ),
   },
   {
+    label: "Vouchers",
+    to: `${ROUTER_URL.ADMIN}/${ROUTER_URL.ADMIN_ROUTES.VOUCHERS}`,
+    icon: (
+      <svg
+        className="size-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"
+        />
+      </svg>
+    ),
+  },
+  {
     label: "Product Franchise",
     to: `${ROUTER_URL.ADMIN}/${ROUTER_URL.ADMIN_ROUTES.PRODUCT_FRANCHISES}`,
     icon: (
